82b00e0f0a357ad702c82035a005e810baa76453
oriolraventosc <oriol.raventos.c@gmail.com>
2023-05-05 19:16:06 +0200

    Delete unused imports/variables

--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,14 +1,10 @@
-import { useParams } from "react-router-dom";
-import useProduct from "../../hooks/useProduct/useProduct";
 import { useAppSelector } from "../../redux/hooks";
 import Loader from "../../components/Loader/Loader";
 
-import { useEffect } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import ProductInformation from "../../components/ProductInformation/ProductInformation";
 
 const ProductPage = (): JSX.Element => {
-  const { id } = useParams();
   const product = useAppSelector((state) => state.productActions.product);
   const isLoading = useAppSelector((state) => state.uiActions.loading);
 
